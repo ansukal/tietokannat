66559d59ab863d31c049ae7cfcc4601bfe5a1614
ansukal <kailaanmaki@live.fi>
2024-03-14 09:36:17 +0200

    ylös nappi

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ function toggleNav() {
   // Piilota togglenavi, jos se on avoinna
   $('.navbar-collapse').collapse('hide');
 }
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) { // Näytä ylös-nappi, kun sivua on vieritetty 100px alaspäin
+          $('.back-to-top').addClass('show');
+      } else {
+          $('.back-to-top').removeClass('show');
+      }
+  });
+
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},800); // Animaatio vierittää sivun ylös
+      return false;
+  });
+});
