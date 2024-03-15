e11bfb6f54b1bd2a5c6597d37d234fa90a5f9d3a
ansukal <kailaanmaki@live.fi>
2024-03-15 13:36:02 +0200

    muutoksia

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ function toggleNav() {
   $('.navbar-collapse').collapse('hide');
 }
 
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+// Kun käyttäjä vierittää 50 pikseliä alaspäin sivulla, näytä nappi
+window.onscroll = function() {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 50 ||
-    document.documentElement.scrollTop > 50
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
+
+// Kun käyttäjä klikkaa nappia, vieritä ylös
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
