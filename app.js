4bfc573dec5c43267e88dec110f50b0a441722aa
ansukal <kailaanmaki@live.fi>
2024-03-15 13:27:16 +0200

    muutoksia

--- app.js
+++ app.js
@@ -3,18 +3,3 @@ function toggleNav() {
   $('.navbar-collapse').collapse('hide');
 }
 
-$(document).ready(function () {
-  // Näytä tai piilota ylös-nappi sen mukaan, onko käyttäjä jo ylhäällä vai ei
-  $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) { // Jos käyttäjä on vierittänyt yli 100 pikseliä
-          $('#scrollToTopBtn').fadeIn();
-      } else {
-          $('#scrollToTopBtn').fadeOut();
-      }
-  });
-
-  // Kun käyttäjä klikkaa ylös-nappia, vieritä ylös
-  function scrollToTop() {
-      $('html, body').animate({ scrollTop: 0 }, 'slow');
-  }
-});
